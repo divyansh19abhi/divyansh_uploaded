8a7b112f26d906b73fee176dffc9fdfd880a7842
divyansh19abhi <divyanshdoonmarch@gmail.com>
2026-08-22 16:00:58 +0530

    recursion prime number

---
+++ primerecursion.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,count=0;
+    cout<<"enter a number ";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        if(n%i==0)
+         count++; 
+    }
+    if(count==2)
+      cout<<"prime number";
+    else
+      cout<<"not a prime";
+      return 0;  
+}
