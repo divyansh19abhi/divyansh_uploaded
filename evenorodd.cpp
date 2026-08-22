e7c927737003bd03a711cb4962ff2dabba2977c5
divyansh19abhi <divyanshdoonmarch@gmail.com>
2026-08-22 10:08:46 +0530

    new changes

---
+++ evenorodd.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter number to be check ";
+    cin>>n;
+    if(n/2==0)
+      cout<<"number is even ";
+    else
+      cout<<"number is odd ";
+    return 0;    
+}
